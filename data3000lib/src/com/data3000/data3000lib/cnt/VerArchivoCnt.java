86626f7f3a73b663fbcd9a6775bd2b3108a91282
hectormao <hectormao@kchsoft>
2017-05-29 19:45:01 -0500

    Ajustes de visualización de archivos

--- data3000lib/src/com/data3000/data3000lib/cnt/VerArchivoCnt.java
+++ data3000lib/src/com/data3000/data3000lib/cnt/VerArchivoCnt.java
@@ -1,6 +1,13 @@
 package com.data3000.data3000lib.cnt;
 
 
+import java.net.FileNameMap;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zul.Iframe;
 import org.zkoss.zul.Window;
 
@@ -20,7 +27,19 @@ public class VerArchivoCnt extends WindowComposer {
 		super.doAfterCompose(winVerArchivo);
 		version = (DocArchivoVersion) argumentos.get(ConstantesAdmin.ARG_SELECCION);
 		if(version != null){
-			frmContenido.setSrc(new StringBuilder("GetArchivo?idVersion=").append(version.getArchVersIdn()).toString());
+			Path path = Paths.get(version.getArchVersRuta());
+			String mimeType = Files.probeContentType(path);
+			String url = null;
+			if(mimeType == null || (mimeType != null && mimeType.toLowerCase().startsWith("image/")) ){
+				url = new StringBuilder("GetArchivo?idVersion=").append(version.getArchVersIdn()).toString();
+			} else {
+				url = "https://docs.google.com/gview?url=" + Executions.getCurrent().getScheme() + "://" + Executions.getCurrent().getServerName() + ":" + Executions.getCurrent().getServerPort() + Executions.getCurrent().getContextPath() + "/GetArchivo?idVersion=" + version.getArchVersIdn() + "&embedded=true";				
+			}
+			
+			frmContenido.setSrc(url);
+			
+			
+			
 		}
 		
 	}
